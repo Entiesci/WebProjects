4437c13998be8aa08127268bea794fb0391dd985
Erica_ntsc <88816192+Entiesci@users.noreply.github.com>
2024-10-28 11:39:29 +0800

    Update gen.part.html.cpp

--- liberliber/gen.part.html.cpp
+++ liberliber/gen.part.html.cpp
@@ -14,7 +14,7 @@ signed main(){
     freopen("part.html.txt","w",stdout);
     while(cin>>title>>author>>url){
         cerr<<title<<author<<url<<endl;
-        cout<<"<div class=\"swiper-slide\"><div class=\"product-card\"><div class=\"image-holder\"><img src=\"img/blbl.jpg\" alt=\"product-item\" class=\"img-fluid\"></div><div class=\"card-detail text-center pt-3 pb-2\"><h5 class=\"card-title fs-4 text-uppercase m-0\"><a href=\""<<url<<"\">"<<title<<"</a></h5><div class=\"cart-button mt-1\">"<<author<<"</div></div></div></div>\n";
+        cout<<"<div class=\"swiper-slide\"><div class=\"product-card\"><div class=\"image-holder\"><img src=\"img/blbl.jpg\" alt=\"product-item\" class=\"img-fluid\"></div><div class=\"card-detail text-center pt-3 pb-2\"><h5 class=\"card-title fs-4 text-uppercase m-0\"><a href=\""<<url<<"\" target=\"_blank\">"<<title<<"</a></h5><div class=\"cart-button mt-1\">"<<author<<"</div></div></div></div>\n";
     }
     return 0;
 }
