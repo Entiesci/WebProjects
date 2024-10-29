80eebe1ff3975786a2a6d4261a4172cdf8dbf9e8
Erica_ntsc <88816192+Entiesci@users.noreply.github.com>
2024-10-29 21:53:12 +0800

    update gen

--- liberliber/gen.set.cpp
+++ liberliber/gen.set.cpp
@@ -10,8 +10,8 @@ signed main(){
 //    res=getchar();
 //    if(res=='N')exit(0);
 
-    freopen("database.db.txt","r",stdin);
-    freopen("index_html.part.txt","w",stdout);
+    freopen("database.set.txt","r",stdin);
+    freopen("html.set.txt","w",stdout);
     while(cin>>title>>author>>url){
         // cerr<<title<<author<<url<<endl;
         cout<<"<div class=\"swiper-slide\"><div class=\"product-card\"><div class=\"image-holder\"><img src=\"img/blbl.jpg\" alt=\"product-item\" class=\"img-fluid\"></div><div class=\"card-detail text-center pt-3 pb-2\"><h5 class=\"card-title fs-4 text-uppercase m-0\"><a href=\""<<url<<"\" target=\"_blank\">"<<title<<"</a></h5><div class=\"cart-button mt-1\">"<<author<<"</div></div></div></div>\n";
--- liberliber/gen.video.cpp
+++ liberliber/gen.video.cpp
@@ -10,8 +10,8 @@ signed main(){
 //    res=getchar();
 //    if(res=='N')exit(0);
 
-    freopen("database.db.txt","r",stdin);
-    freopen("all_html.part.txt","w",stdout);
+    freopen("database.video.txt","r",stdin);
+    freopen("html.video.txt","w",stdout);
     while(cin>>tit>>aut>>url){
         // cerr<<tit<<aut<<url<<endl;
         cout<<"<div class=\"col-md-4 mb-2\"><div class=\"card border-0 rounded-0\"><div class=\"card-image\"><img src=\"img/blbl.jpg\" alt=\"blog-img\" class=\"img-fluid\"></div></div><div class=\"card-body text-capitalize\"><div class=\"card-meta fs-6\"><span class=\"meta-category\"><a href=\"https://search.bilibili.com/all?keyword="<<aut<<"\">"<<aut<<"</a></span></div><h4 class=\"card-tit\"><a href=\""<<url<<"\" target=\"_blank\">"<<tit<<"</a></h4></div></div>\n";
